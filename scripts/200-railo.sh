fcd79fcf0a2858e78b9a73856cffd6a395b5d623
Pete Freitag <pete@foundeo.com>
2014-11-12 13:06:27 -0500

    Exit if railo download fails

--- scripts/200-railo.sh
+++ scripts/200-railo.sh
@@ -10,5 +10,13 @@ mkdir /opt/railo/config
 mkdir /opt/railo/config/server
 mkdir /opt/railo/config/web
 curl -o /opt/railo/railo.tar.gz $railo
+
+if [ -f "/opt/railo/railo.tar.gz" ]; then
+  echo "Download Complete"
+else
+  echo "Download of Railo Failed Exiting..."
+  exit 1
+fi
+
 tar -xzf /opt/railo/railo.tar.gz -C /opt/railo
 ln -s /opt/railo/$railo_folder /opt/railo/current
