3f859dad6bffe6352da00cd1daaeaccb52fe60db
Alexander Ambrose <alexa@fh-group.com>
2016-06-22 15:58:57 -0400

    Initial changes for 16.04 and Lucee 5
    Copy the changes over from Ansible scripts to shell scripts. Mainly
    configuration changes, but also changes related to only a single JAR
    file now.

--- scripts/200-lucee.sh
+++ scripts/200-lucee.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jar_url="https://bitbucket.org/lucee/lucee/downloads/lucee-$LUCEE_VERSION-jars.zip"
+jar_url="http://cdn.lucee.org/rest/update/provider/loader/$LUCEE_VERSION"
 jar_folder="lucee-$LUCEE_VERSION"
 
 echo "Installing Lucee"
@@ -10,7 +10,7 @@ mkdir /opt/lucee/config
 mkdir /opt/lucee/config/server
 mkdir /opt/lucee/config/web
 mkdir /opt/lucee/$jar_folder
-curl --location -o /opt/lucee/lucee.zip $jar_url
+curl --location -o /opt/lucee/$jar_folder/lucee.jar $jar_url
 
 if [ -f "/opt/lucee/lucee.zip" ]; then
   echo "Download Complete"
@@ -19,5 +19,4 @@ else
   exit 1
 fi
 
-unzip /opt/lucee/lucee.zip -d /opt/lucee/$jar_folder
 ln -s /opt/lucee/$jar_folder /opt/lucee/current
